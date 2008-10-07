75a80156289a6e752cccb207cc12d7ccd7219f37
Konstantin Baev <kipruss@altlinux.org>
2008-10-07 14:05:23 +0400

    Added usage Generic for etercifs sources

--- etercifs/functions.sh
+++ etercifs/functions.sh
@@ -19,16 +19,30 @@ get_sd()
 {
     BASE_KERNEL_SOURCES_DIR=
     BASE_KERNEL_SOURCES_DIR=`grep -i $1 kernel_src.list | head -n1 | cut -d" " -f 2 2>/dev/null`
+}
+
+get_etercifs_sd()
+{
     ETERCIFS_SOURCES_LIST=
     ETERCIFS_SOURCES_LIST=`grep -i $1 etercifs_src.list | head -n1 | cut -d" " -f 2 2>/dev/null`
 }
+
 
 get_src_dir()
 {
     get_sd `$DISTR_VENDOR -e`
     [ -z "$BASE_KERNEL_SOURCES_DIR" ] && get_sd `$DISTR_VENDOR -d`
-    [ -z "$BASE_KERNEL_SOURCES_DIR" ] && { echo "Unknown `$DISTR_VENDOR -d`, use Generic" ; get_sd Generic ; }
+    [ -z "$BASE_KERNEL_SOURCES_DIR" ] && { echo "Unknown `$DISTR_VENDOR -d`, use Generic for kernel sources" ; get_sd Generic ; }
     [ -z "$BASE_KERNEL_SOURCES_DIR" ] && return 1
+    return 0
+}
+
+get_etercifs_src_dir()
+{
+    get_etercifs_sd `$DISTR_VENDOR -e`
+    [ -z "$ETERCIFS_SOURCES_LIST" ] && get_etercifs_sd `$DISTR_VENDOR -d`
+    [ -z "$ETERCIFS_SOURCES_LIST" ] && { echo "Unknown `$DISTR_VENDOR -d`, use Generic for etercifs sources" ; get_etercifs_sd Generic ; }
+    [ -z "$ETERCIFS_SOURCES_LIST" ] && return 1
     return 0
 }
 
