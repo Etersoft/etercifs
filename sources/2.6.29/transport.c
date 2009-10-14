3910223fddc9da2ac8cb7da81e286f2408866da8
Evgeny Sinelnikov <sin@altlinux.ru>
2009-10-14 13:43:40 +0400

    Fixed fd duplicate problem with locks for 2.6.29

--- sources/2.6.29/transport.c
+++ sources/2.6.29/transport.c
@@ -104,7 +104,7 @@ DeleteMidQEntry(struct mid_q_entry *midEntry)
 }
 
 struct oplock_q_entry *
-AllocOplockQEntry(struct inode *pinode, __u16 fid, struct cifsTconInfo *tcon)
+AllocOplockQEntry(struct inode *pinode, __u16 fid, __u32 pid, struct cifsTconInfo *tcon)
 {
 	struct oplock_q_entry *temp;
 	if ((pinode == NULL) || (tcon == NULL)) {
@@ -119,6 +119,7 @@ AllocOplockQEntry(struct inode *pinode, __u16 fid, struct cifsTconInfo *tcon)
 		temp->pinode = pinode;
 		temp->tcon = tcon;
 		temp->netfid = fid;
+		temp->netpid = pid;
 		spin_lock(&GlobalMid_Lock);
 		list_add_tail(&temp->qhead, &GlobalOplock_Q);
 		spin_unlock(&GlobalMid_Lock);
