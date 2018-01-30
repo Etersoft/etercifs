ac75de6b2b847df3519bb5f20413f0975c504e39
Vitaly Lipatov <lav@etersoft.ru>
2018-01-30 14:31:51 +0300

    update_git.sh: add update local sources too (with -u)

--- develop/update_git.sh
+++ develop/update_git.sh
@@ -8,16 +8,50 @@ fatal()
     exit 1
 }
 
+SOURCESDIR=$(realpath ../sources)
 ECSOURCE=../../etercifs-source
 MERGEOPT="--no-edit"
 
+UPDATE=''
+if [ "$1" = "-u" ] ; then
+    UPDATE="$1"
+    shift
+fi
+
 MV="$1"
-[ -n "$MV" ] || fatal "Run with version, f.i. ./update_git.sh 4.11"
+[ -n "$MV" ] || fatal "Run with version, f.i. ./update_git.sh 4.11 or ./update_git.sh -u 4.11 for update local sources too"
 
 KTAG=v$MV
 KBRANCH=linux-$MV.y
 
 ELIST="etersoft-common etersoft-share-flags etersoft-wine"
+
+update_branch()
+{
+    LASTTAG=$(git describe --tags upstream/$KBRANCH) || fatal
+    echo "Just update branch $KTAG-etercifs to latest commit from $KBRANCH with tag $LASTTAG"
+    git checkout $KTAG-etercifs || fatal
+    #git merge $MERGEOPT upstream/$KBRANCH || fatal
+    git merge $MERGEOPT $LASTTAG || fatal
+}
+
+update_sources()
+{
+    [ -n "$UPDATE" ] || return
+    local MV="$1"
+    local LASTTAG="$2"
+
+    echo "Copying sources from branch $MV-etercifs to local dir ..."
+
+    local TEXT="update $MV up to $LASTTAG"
+    [ -d "$SOURCESDIR/$MV" ] || TEXT="add $MV branch ($LASTTAG)"
+
+    rm -r "$SOURCESDIR/$MV" || fatal
+    cp -a fs/cifs "$SOURCESDIR/$MV" || fatal
+    cd $SOURCESDIR || fatal
+    git commit $MV -m "$TEXT" || fatal
+}
+
 
 if [ ! -d $ECSOURCE ] ; then
     cd $(dirname $ECSOURCE) || fatal
@@ -32,12 +66,10 @@ git fetch upstream
 
 git tag | grep "$KTAG" || fatal "There is no $KTAG tag in the git repo."
 
+
 if git branch | grep -q $KTAG-etercifs ; then
-    LASTTAG=$(git describe --tags upstream/$KBRANCH) || fatal
-    echo "Just update branch $KTAG-etercifs to latest commit from $KBRANCH with tag $LASTTAG"
-    git checkout $KTAG-etercifs || fatal
-    #git merge $MERGEOPT upstream/$KBRANCH || fatal
-    git merge $MERGEOPT $LASTTAG || fatal
+    update_branch
+    update_sources $MV $LASTTAG
     exit
 fi
 
@@ -56,4 +88,5 @@ for i in $ELIST ; do
     git merge $MERGEOPT $i || fatal
 done
 
-echo "You are in branch $MV-etercifs, copy fs/cifs to "
+update_branch
+update_sources $MV $LASTTAG
